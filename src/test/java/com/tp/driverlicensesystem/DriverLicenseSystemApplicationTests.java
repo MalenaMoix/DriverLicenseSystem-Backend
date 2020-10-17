0e5297e81e20f0dda0da40c2201c95bd3e4a2334
MalenaMoix <moixmalena@gmail.com>
2020-10-17 11:59:16 -0300

    Initial configuration

---
+++ src/test/java/com/tp/driverlicensesystem/DriverLicenseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.driverlicensesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverLicenseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
